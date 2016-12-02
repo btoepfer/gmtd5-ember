4e458f2483fa6a6fa69a1d0aabdaa91a56d73610
Bernd Toepfer <bernd@bc-toepfer.de>
2016-12-02 17:59:04 +0100

    Zugriff aus Rails-API definiert

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,27 +1,7 @@
 import Ember from 'ember';
 
-let tasks = [{
-  id: 7,
-  status: 0,
-  name: "Lorem ipsum dolor 1",
-  content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  id: 8,
-  status: 0,
-  name: "Lorem ipsum 2",
-  content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  id: 9,
-  status: 1,
-  name: "Lorem 3",
-  content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  }
-];
-
 export default Ember.Route.extend({
   model() {
-    return tasks;
+    return this.store.findAll('task');
   }
 });
